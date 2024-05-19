ea6eb8475b53ebd8839c2ebf81e3c97af0fa5b36
도겸 <rlaehrua930@naver.com>
2024-05-19 09:39:17 +0900

    [2446] Title: 별 찍기 - 9, Time: 140 ms, Memory: 14400 KB -NKLCBHub

---
+++ 백준/#2446 별 찍기 - 9/별 찍기 － 9.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i; j++) {
+				bw.write(" ");
+			}
+
+			for (int j = 0; j < (N - i) * 2 - 1; j++) {
+				bw.write("*");
+			}
+			bw.newLine();
+		}
+
+		for (int i = 1; i < N; i++) {
+			for (int j = 0; j < N - i - 1; j++) {
+				bw.write(" ");
+			}
+			for (int j = 0; j < i * 2 + 1; j++) {
+				bw.write("*");
+			}
+			bw.newLine();
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
