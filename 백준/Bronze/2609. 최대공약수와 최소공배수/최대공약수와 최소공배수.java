a251d1f7e827a60f0b538d6c5429237ae81f1291
도겸 <rlaehrua930@naver.com>
2024-11-01 23:42:18 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 112 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -16,7 +16,7 @@ public class Main {
 		m = Integer.parseInt(st.nextToken());
 
 		// 최대 공약수
-		int gcd = gcd(n,m);
+		int gcd = gcd_(n,m);
 		// 최소 공배수
 		int lcm = lcm(n,m, gcd);
 
@@ -25,14 +25,26 @@ public class Main {
 		bw.write(lcm + "");
 		bw.flush();
 		bw.close();
-		
+
 	}
 
+	// 재귀방식
 	static int gcd(int a,int b){
 		if (b == 0) {
 			return a;
 		}
 		return gcd(b,a%b);
+	}
+
+	// 반복문방식
+	static int gcd_(int a,int b){
+		while (b != 0) {
+			int r = a % b;
+
+			a = b;
+			b = r;
+		}
+		return a;
 	}
 
 	static int lcm(int a, int b, int gcd){
